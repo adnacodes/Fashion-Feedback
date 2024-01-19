d5cc695a1fa8d4aca9a8b5a83e85f1cf0e2a0ffa
matnull <arodr1293@fiu.edu>
2024-01-19 15:06:15 -0500

    Add pytrends routes for searching and gathering related search terms

--- backend/api.py
+++ backend/api.py
@@ -1,15 +1,31 @@
 from flask import Flask
 import os
+from pytrends.request import TrendReq
+import pandas as pd
 
 path = os.getcwd()
 parent = os.path.dirname(path)
 
 app = Flask(__name__, template_folder=parent + '/frontend')
-
+pytrends = TrendReq(hl='en-US', tz=360)
 
 @app.route('/')
 def home():
     return {}
+
+# Gather interest over time for a given search term
+@app.route('/api/search/<param>')
+def interest_over_time(param):
+    pytrends.build_payload([param], timeframe='today 5-y')
+    interest_over_time = pytrends.interest_over_time()
+    return interest_over_time.to_json()
+
+# Gathers related queries for a given search term
+@app.route('/api/related/<param>')
+def related(param):
+    pytrends.build_payload([param], timeframe='today 5-y')
+    related_queries = pytrends.related_queries()
+    return related_queries[param]['top'].to_json()
 
 if __name__ == '__main__':
     app.debug = True
