416dcde398166bd6ff73b8bfbec44e5e28253405
Shashank Sinha <shashank.wmr@gmail.com>
2024-01-22 15:36:04 -0500

    Class name change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <div className="relative flex items-center w-full h-10 rounded-full focus-within:shadow-lg bg-white overflow-hidden">
           <div className="grid place-items-center h-full w-12 text-gray-300">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
               </svg>
           </div>
           <input
